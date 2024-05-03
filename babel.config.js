29d2865936ac85be8e26e954b94c845cd80eafd6
brian.h <anhkun123456@gmail.com>
2024-05-03 22:19:42 +0700

    new: absolute import config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@assets": "./assets",
+            "@components": "./src/components",
+            "@navigations": "./src/navigations",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
-import CKButton from "../../components/CKButton";
+import CKButton from "src/components/CKButton";
 
 export default function Login() {
   const handlePress = () => {
