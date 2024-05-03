d88bd627e0f800c37753ab47a2b68f6c99f71561
Nguyễn Hoàng Vũ <hoangvu22062001@gmail.com>
2024-05-03 14:52:08 +0700

    CkButton Component

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,9 +1,14 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import CKButton from "../../components/CKButton";
 
 export default function Login() {
+  const handlePress = () => {
+    console.log('Button pressed!');
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={{ marginTop: 100 }}>
+      <CKButton title="Tiếp tục" onPress={handlePress} variant='main' />
     </View>
   );
 }
