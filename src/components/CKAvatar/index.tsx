dd6db780dbcea393b2c0f6710c6c1cccfa46d43a
brian.h <anhkun123456@gmail.com>
2024-05-03 22:35:57 +0700

    new: init ckavatar

---
+++ src/components/CKAvatar/index.tsx
@@ -0,0 +1,11 @@
+import { Image, View } from "react-native";
+
+interface ICKAvatarProps {}
+
+export default function CKAvatar({}: ICKAvatarProps) {
+  return (
+    <View>
+      <Image />
+    </View>
+  );
+}
