71daf6d4a3ff889406cf9888f71307d584f36ea3
Nguyễn Hoàng Vũ <hoangvu22062001@gmail.com>
2024-05-04 15:21:41 +0700

    new: CKAvatar component

--- src/components/CKAvatar/index.tsx
+++ src/components/CKAvatar/index.tsx
@@ -1,11 +1,46 @@
-import { Image, View } from "react-native";
+import React from 'react';
+import { TouchableOpacity, Image, StyleSheet, TouchableOpacityProps, StyleProp, ViewStyle } from 'react-native';
 
-interface ICKAvatarProps {}
+type CKAvatarSize = 'sm' | 'md' | 'lg';
+interface ICKAvatarProps extends TouchableOpacityProps {
+  uri?: string;
+  size?: CKAvatarSize;
+  onPress: () => void;
+}
 
-export default function CKAvatar({}: ICKAvatarProps) {
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 50,
+    overflow: 'hidden',
+  },
+  avatar: {
+    width: '100%',
+    height: '100%',
+  },
+  small: {
+    width: 40,
+    height: 40,
+  },
+  medium: {
+    width: 60,
+    height: 60,
+  },
+  large: {
+    width: 80,
+    height: 80,
+  }
+});
+
+const avatarStyle: { [key in CKAvatarSize]: StyleProp<ViewStyle> } = {
+  sm: styles.small,
+  md: styles.medium,
+  lg: styles.large,
+};
+
+export default function CKAvatar({ uri, size = 'sm', onPress, style, ...props }: ICKAvatarProps) {
   return (
-    <View>
-      <Image />
-    </View>
+    <TouchableOpacity onPress={onPress} style={[styles.container, avatarStyle[size]]} {...props}>
+      <Image source={{ uri }} style={styles.avatar} />
+    </TouchableOpacity>
   );
 }
